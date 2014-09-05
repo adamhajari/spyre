2939888aa71423dd75e16c46d4611abaaacb2e65
Adam Hajari <adamhajari@gmail.com>
2014-09-05 01:06:02 -0400

    made it possible to add multiple plot functions, table types, or html inserts

--- spyre/model.py
+++ spyre/model.py
@@ -7,7 +7,7 @@ class Plot:
 	def getPlotPath(self, plt_obj):
 		buffer = StringIO.StringIO()
 		if type(plt_obj).__name__ == 'Figure':
-			plt_obj.savefig(buffer,format='png')
+			plt_obj.savefig(buffer,format='png',bbox_inches='tight')
 		# if type(plt_obj).__name__ == 'ggplot':
 		# 	ggplot.ggsave(plt_obj,filename=buffer,format='png')
 		return(buffer)
