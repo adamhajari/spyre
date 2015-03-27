ad8d99c1249f00e76083f4e7717b0f5f0b5b2f84
Adam Hajari <adamjdanger@gmail.com>
2015-03-27 12:13:38 -0400

    added testing

---
+++ tests/test_app.py
@@ -0,0 +1,156 @@
+# from spyre import server
+from spyre import server
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from numpy import pi
+
+class TestApp(server.App):
+	colors = [
+	{"label":"Green", "value":'g'},
+    {"label": "Red", "value":'r'}, 
+    {"label": "Blue", "value":'b'}, 
+    {"label": "Yellow", "value":'y'},
+    ]
+
+	title = "Simple Sine Wave"
+	inputs = [{	"input_type":'text',
+				"label": 'Title', 
+				"value" : 'Simple Sine Wave',
+				"variable_name": 'title', 
+				"action_id" : "plot",
+			},
+			{	"input_type":'radiobuttons',
+				"label": 'Function', 
+				"options" : [
+					{"label": "Sine", "value":"sin", "checked":True}, 
+					{"label":"Cosine", "value":"cos"}
+				],
+				"variable_name": 'func_type', 
+				"action_id" : "plot",
+			},
+			{	"input_type":'checkboxgroup',
+				"label": 'Axis Labels', 
+				"options" : [
+					{"label": "x-axis", "value":1, "checked":True}, 
+					{"label":"y-axis", "value":2}
+				],
+				"variable_name": 'axis_label', 
+				"action_id" : "plot",
+			},
+			{	"input_type":'dropdown',
+				"label": 'Line Color', 
+				"options" : colors,
+				"variable_name": 'color', 
+				"action_id" : "plot",
+			},
+			{	"input_type":'slider',
+				"label": 'frequency', 
+				"variable_name": 'freq', 
+				"value" : 2,
+				"min" : 1, 
+				"max" : 30,
+				"action_id" : "plot",
+			}]
+	controls = [{	"control_type" : "button",
+					"control_id" : "button1",
+					"label" : "plot",
+				},
+				{	"control_type" : "button",
+					"control_id" : "button2",
+					"label" : "download",
+				}]
+	outputs = [{	"output_type" : "html",
+					"output_id" : "html_id",
+					"control_id" : "button1",
+					"on_page_load" : True,
+				},
+				{	"output_type" : "plot",
+					"output_id" : "plot",
+					"control_id" : "button1",
+					"on_page_load" : True,
+				},
+				{	"output_type" : "plot",
+					"output_id" : "plot2",
+					"control_id" : "button1",
+					"on_page_load" : True,
+				},
+				{	"output_type" : "table",
+					"output_id" : "table_id",
+					"control_id" : "button1",
+					"sortable" : True,
+					"on_page_load" : True,
+				},
+				{	"output_type" : "download",
+					"output_id" : "download_id",
+					"control_id" : "button2",
+				}]
+
+	def getPlot(self, params):
+		fig = plt.figure()  # make figure object
+		splt = fig.add_subplot(1,1,1)
+
+		f = float(params['freq'])
+		title = params['title']
+		axis_label = map( int, params['axis_label'] )
+		color = params['color']
+		func_type = params['func_type']
+
+		x = np.arange(0,6*pi,pi/50)
+		splt.set_title(title)
+		for axis in axis_label:
+			if axis==1:
+				splt.set_xlabel('x axis')
+			if axis==2:
+				splt.set_ylabel('y axis')
+		if func_type=='cos':
+			y = np.cos(f*x)
+		else:
+			y = np.sin(f*x)
+		splt.plot(x,y,color=color)  # sine wave
+		return fig
+
+	def plot(self):
+		fig = plt.figure()  # make figure object
+		splt = fig.add_subplot(1,1,1)
+
+		f = float(params['freq'])
+		title = params['title']
+		axis_label = map( int, params['axis_label'] )
+		color = params['color']
+		func_type = params['func_type']
+
+		x = np.arange(0,6*pi,pi/50)
+		splt.set_title(title)
+		for axis in axis_label:
+			if axis==1:
+				splt.set_xlabel('x axis')
+			if axis==2:
+				splt.set_ylabel('y axis')
+		if func_type=='cos':
+			y = np.cos(f*x)
+		else:
+			y = np.sin(f*x)
+		splt.plot(x,y,color=color)  # sine wave
+		return fig
+
+	def plot2(self):
+		fig = plt.figure()  # make figure object
+		splt = fig.add_subplot(1,1,1)
+		x = np.arange(0,6*pi,pi/50)
+		y = np.sin(f*x)
+		splt.plot(x,y)  # sine wave
+		return fig
+
+	def html_id(self):
+		return "hello world"
+
+	def getData(self,params):
+		count = [1,4,3]
+		name = ['<a href="http://adamhajari.com">A</a>','B','C']
+		df = pd.DataFrame({'name':name, 'count':count})
+		return df
+
+	def noOutput(self, input_params):
+		return 0
---
+++ tests/tests.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import cherrypy
+
+from cptestcase import BaseCherryPyTestCase
+from test_app import TestApp
+import json
+
+def setUpModule():
+    app = TestApp()
+    cherrypy.tree.mount(app.getRoot(), '/')
+    cherrypy.engine.start()
+setup_module = setUpModule
+
+def tearDownModule():
+    cherrypy.engine.exit()
+teardown_module = tearDownModule
+
+class TestCherryPyApp(BaseCherryPyTestCase):
+    def test_index(self):
+        response = self.request('/')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+        # response body is wrapped into a list internally by CherryPy
+        # self.assertEqual(response.body, ['hello world'])
+
+    def test_html(self):
+        response = self.request('/html', output_id='html_id')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+        self.assertEqual(response.body[0].decode('utf8'), "hello world")
+
+    def test_table(self):
+        response = self.request('/table', ex_first='__float__0', output_id='table_id')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+        self.assertTrue(response.body[0].decode('utf8').replace(" ","").find("<td>4</td>")>=0)
+
+    def test_data(self):
+        response = self.request('/data', ex_first='__float__0', output_id='table_id')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+        resp_dict = json.loads(response.body[0].decode('utf8'))
+        self.assertTrue(len(resp_dict['data'])==3)
+
+    def test_plot(self):
+        response = self.request('/plot', title='t', func_type='sin', axis_label='__list__,1', color='g', freq='__float__2',  output_id='plot')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+
+    def test_image(self):
+        response = self.request('/image', ex_first='__float__0', output_id='image')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+
+    def test_download(self):
+        response = self.request('/download', ex_first='__float__0')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+
+    def test_no_output(self):
+        response = self.request('/no_output')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+
+    def test_spinner(self):
+        response = self.request('/spinning_wheel')
+        self.assertEqual(response.output_status.decode('utf8'), '200 OK')
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
