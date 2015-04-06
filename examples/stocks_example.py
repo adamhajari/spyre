488d9fdd8b95273853595a006ae3f50ac0038c03
Adam Hajari <adamjdanger@gmail.com>
2015-04-06 12:38:03 -0400

    added a caching layer

--- examples/stocks_example.py
+++ examples/stocks_example.py
@@ -10,6 +10,12 @@ except ImportError:
 	import urllib.request as urllib2
 
 class StockExample(server.App):
+
+	def __init__(self):
+		# implements a simple caching mechanism to avoid multiple calls to the yahoo finance api
+		self.data_cache = None
+		self.params_cache = None
+
 	title = "Historical Stock Prices"
 
 	inputs = [{		"input_type":'dropdown',
@@ -38,17 +44,26 @@ class StockExample(server.App):
 					"on_page_load" : True }]
 
 	def getData(self, params):
-		ticker = params['ticker']
-		# make call to yahoo finance api to get historical stock data
-		api_url = 'https://chartapi.finance.yahoo.com/instrument/1.0/{}/chartdata;type=quote;range=3m/json'.format(ticker)
-		result = urllib2.urlopen(api_url).read()
-		data = json.loads(result.decode('utf-8').replace('finance_charts_json_callback( ','')[:-1])  # strip away the javascript and load json
-		self.company_name = data['meta']['Company-Name']
-		df = pd.DataFrame.from_records(data['series'])
-		df['Date'] = pd.to_datetime(df['Date'],format='%Y%m%d')
-		return df
+		params.pop("output_id",None)
+		if self.params_cache!=params:
+			ticker = params['ticker']
+			# make call to yahoo finance api to get historical stock data
+			api_url = 'https://chartapi.finance.yahoo.com/instrument/1.0/{}/chartdata;type=quote;range=3m/json'.format(ticker)
+			result = urllib2.urlopen(api_url).read()
+			data = json.loads(result.decode('utf-8').replace('finance_charts_json_callback( ','')[:-1])  # strip away the javascript and load json
+			self.company_name = data['meta']['Company-Name']
+			df = pd.DataFrame.from_records(data['series'])
+			df['Date'] = pd.to_datetime(df['Date'],format='%Y%m%d')
+			self.data_cache = df
+			self.params_cache = params
+		return self.data_cache
 
 	def getPlot(self, params):
+		### implements a simple caching mechanism to avoid multiple calls to the yahoo finance api ###
+		params.pop("output_id",None)
+		while self.params_cache!=params:
+			pass
+		###############################################################################################
 		df = self.getData(params)
 		plt_obj = df.set_index('Date').drop(['volume'],axis=1).plot()
 		plt_obj.set_ylabel("Price")
