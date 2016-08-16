ea8cebd3af2496aff509515c9f2070578a34f667
Adam Hajari <adamhajari@gmail.com>
2016-08-16 12:57:30 -0400

    moved update trigger from search element to query function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DataSpyre',
-    version='0.2.5',
+    version='0.2.6',
     description='Spyre makes it easy to build interactive web applications, and requires no knowledge of HTML, CSS, or Javascript.',
     url='https://github.com/adamhajari/spyre',
     author='Adam Hajari',
