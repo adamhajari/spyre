9c9caa9f4189c7d2e2b72e703e07e076b8145377
Islam Elnabarawy <islam.ossama@gmail.com>
2017-06-15 11:59:09 -0500

    Fix python lint error

--- spyre/model.py
+++ spyre/model.py
@@ -14,7 +14,8 @@ class Plot:
         if isinstance(plt_obj, matplotlib.axes.Axes):
             plt_obj.get_figure().savefig(buffer, format='png', bbox_inches='tight')
         else:
-            logging.error("Error: getPlot method must return an pyplot figure or matplotlib Axes object")
+            logging.error("Error: getPlot method must return an pyplot figure "
+                          "or matplotlib Axes object")
         plt.close('all')
         return(buffer)
 
