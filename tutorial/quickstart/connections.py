246db36b66f652097b49ecd457ef558598438194
Adam Hajari <adamhajari@gmail.com>
2017-11-07 17:17:24 -0500

    clean up tutorials directory

---
+++ tutorial/quickstart/connections.py
@@ -0,0 +1,50 @@
+from spyre import server
+import matplotlib.image as mpimg
+
+
+class InputExample(server.Launch):
+
+    title = "Connections"
+    inputs = [{
+        "input_type": 'radiobuttons',
+        "options": [
+            {"label": "Simple App", "value": 1, "checked": True},
+            {"label": "Multiple Outputs", "value": 2},
+            {"label": "Inputs with actions 1", "value": 3},
+            {"label": "Inputs with actions 2", "value": 4},
+        ],
+        "variable_name": 'slide_selector',
+        "action_id": "image_output",
+    }]
+    controls = [{
+        "control_type": "hidden",
+        "control_id": "button1",
+        "label": "Button",
+    }]
+    outputs = [{
+        "output_type": "image",
+        "output_id": "image_output",
+        "control_id": "button1",
+        "on_page_load": True,
+    }]
+
+    def getImage(self, params):
+        slide_selector = int(params['slide_selector'])
+        absolute_path = ''
+        if slide_selector == 2:
+            img = mpimg.imread(absolute_path + 'slide2.png')
+        elif slide_selector == 3:
+            img = mpimg.imread(absolute_path + 'slide3.png')
+        elif slide_selector == 4:
+            img = mpimg.imread(absolute_path + 'slide4.png')
+        else:
+            img = mpimg.imread(absolute_path + 'slide1.png')
+
+        return img
+
+    def noOutput(self, input_params):
+        pass
+
+
+app = InputExample()
+app.launch(port=9096)
---
+++ tutorial/quickstart/stocks_example_no_internet.py
@@ -0,0 +1,62 @@
+# tested with python2.7 and 3.4
+from spyre import server
+
+import pickle
+
+try:
+    import urllib2
+except ImportError:
+    import urllib.request as urllib2
+
+
+class StockExample(server.App):
+
+    title = "Historical Stock Prices"
+
+    inputs = [{
+        "input_type": 'radiobuttons',
+        "label": 'Company',
+        "options": [
+            {"label": "Google", "value": "GOOG"},
+            {"label": "Yahoo", "value": "YHOO"},
+            {"label": "Apple", "value": "AAPL"}],
+        "variable_name": 'ticker',
+        "action_id": "update_data"
+    }]
+
+    controls = [{
+        "control_type": "hidden",
+        "label": "get historical stock prices",
+        "control_id": "update_data"
+    }]
+
+    tabs = ["Plot", "Table"]
+
+    outputs = [{
+        "output_type": "plot",
+        "output_id": "plot",
+        "control_id": "update_data",
+        "tab": "Plot"
+    }, {
+        "output_type": "table",
+        "output_id": "table_id",
+        "control_id": "update_data",
+        "tab": "Table"
+    }]
+
+    def getData(self, params):
+        dfs = pickle.load((open("stocks.pkl", "rb")))
+        ticker = params['ticker']
+        return dfs[ticker]
+
+    def getPlot(self, params):
+        df = self.getData(params)
+        plt_obj = df.set_index('Date').drop(['volume'], axis=1).plot()
+        plt_obj.set_ylabel("Price")
+        fig = plt_obj.get_figure()
+        return fig
+
+
+if __name__ == '__main__':
+    app = StockExample()
+    app.launch(port=9093)
